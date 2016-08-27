885856c20bc968bfeced1513689f065b366ad65b
NIkolayrr <niki_r95@yahoo.com>
2016-08-27 13:00:25 +0300

    Added styles

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+(function () {
+    const kinveyBaseUrl = 'https://baas.kinvey.com/';
+    const kinveyAppID = 'kid_HJQwButH';
+    const kinveyAppSecret = '29c001eed21144b28ac79cd4716643dc';
+    
+    
+});
